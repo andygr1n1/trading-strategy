4479c41db7775e830e4ba6e863716435e9ca621b
Grini Andrei <andrei.grini@carasent.com>
2024-05-05 20:04:33 +0100

    feat: atr

---
+++ components/article/ABlockTitle.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+defineProps<{
+  text: string
+}>()
+
+</script>
+
+<template>
+  <a-divider orientation="left" style="height: 60px; border-color: snow">
+    <h4
+      class="text-gray-300">
+      {{ text }}
+    </h4>
+  </a-divider>
+</template>
